eb6e4d404f92a7a4f2b7f2658481c5600da3011d
veyselo <veysel.oguz@vestel.com.tr>
2020-05-26 16:27:49 +0300

    Failed attempt operation is detected

--- app/src/main/java/com/journaldev/androidmvvmbasics/model/User.java
+++ app/src/main/java/com/journaldev/androidmvvmbasics/model/User.java
@@ -3,16 +3,29 @@ package com.journaldev.androidmvvmbasics.model;
 import androidx.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Patterns;
+import com.journaldev.androidmvvmbasics.views.MainActivity;
+
+import com.journaldev.androidmvvmbasics.views.MainActivity;
 
 public class User {
     @NonNull
     private String mEmail;
     @NonNull
     private String mPassword;
+    private static int failedAttempt = 0;
+    private static int MAX_ATTEMPT = 5;
 
     public User(@NonNull final String email, @NonNull final String password) {
         mEmail = email;
         mPassword = password;
+        failedAttempt = 0;
+    }
+
+    public static void setFailedAttempt() {
+        failedAttempt++;
+        if ( failedAttempt == MAX_ATTEMPT ) {
+            MainActivity.DisableLogin();
+        }
     }
 
     @NonNull
--- app/src/main/java/com/journaldev/androidmvvmbasics/views/MainActivity.java
+++ app/src/main/java/com/journaldev/androidmvvmbasics/views/MainActivity.java
@@ -4,6 +4,8 @@ package com.journaldev.androidmvvmbasics.views;
 import androidx.databinding.BindingAdapter;
 import androidx.databinding.DataBindingUtil;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -12,10 +14,16 @@ import android.os.Handler;
 
 import com.journaldev.androidmvvmbasics.R;
 import com.journaldev.androidmvvmbasics.databinding.ActivityMainBinding;
+import com.journaldev.androidmvvmbasics.model.User;
 import com.journaldev.androidmvvmbasics.viewmodels.LoginViewModel;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String success_m = "Login was successful";
+    private static String error_m = "Email or Password not valid";
+    private static String message_w = "MAX ATTEMPT reached!";
+    public static Context baseContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         activityMainBinding.setViewModel(new LoginViewModel());
         activityMainBinding.executePendingBindings();
+
+        baseContext = getBaseContext();
 
         new Handler().postDelayed(new Runnable() {
             public void run() {
@@ -32,9 +42,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public static void DisableLogin() {
+        Toast.makeText(baseContext, message_w, Toast.LENGTH_SHORT).show();
+    }
+
     @BindingAdapter({"toastMessage"})
     public static void runMe(View view, String message) {
-        if (message != null)
+        if (message != null) {
+
+            if ( message.equals(error_m)) {
+                User.setFailedAttempt();
+            }
             Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
